22762bb8fd223610df7f855a93f54c7d4adeb56d
Sylvaner <sylvain.dangin@gmail.com>
2020-12-05 22:50:58 +0100

    Généralisation du .env

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 git clone https://github.com/Sylvaner/NxDv2-logic logic
+git clone https://github.com/Sylvaner/NxDv2-rest rest
+cp -fr .env logic/.env
+cp -fr .env rest/.env
+docker-compose up -d
